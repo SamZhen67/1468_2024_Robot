0923da9165e62a95f068a7472d71f2bab267342f
SamZhen67 <122785689+SamZhen67@users.noreply.github.com>
2024-01-03 21:05:28 -0500

    Added circle auto and renamed figure 8 auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-// will be used once robot design is developed
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
